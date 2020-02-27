19eaaa5bc33216798cd8c333a51b879b5a9bbda8
rubenaeg <email@aegerter.tech>
2020-02-27 14:14:35 +0100

    :arrow_up: Upgrade capsule configurations, Adjust to Jovo v3

--- src/app.js
+++ src/app.js
@@ -14,52 +14,51 @@ const { Bixby } = require('jovo-platform-bixby');
 const app = new App();
 
 app.use(
-    new Alexa(),
-    new Bixby(),
-    new GoogleAssistant(),
-    new JovoDebugger(),
-    new FileDb()
+	new Alexa(),
+	new Bixby(),
+	new GoogleAssistant(),
+	new JovoDebugger(),
+	new FileDb()
 );
-
 
 // ------------------------------------------------------------------
 // APP LOGIC
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        console.log('LAUNCH()');
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
-    },
+	LAUNCH() {
+		console.log('LAUNCH()');
+		this.ask("Hello World! What's your name?", 'Please tell me your name.');
+	},
 
-    MyNameIsIntent() {
-        console.log('MyNameIsIntent()');
-        this.$bixbyCapsule.$layout.single = 'Hello World.';
-        this.$session.$data.name = this.$inputs.name.value;
-        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-    },
+	MyNameIsIntent() {
+		console.log('MyNameIsIntent()');
+		this.$bixbyCapsule.$layout.single = 'Hello World.';
+		this.$session.$data.name = this.$inputs.name.value;
+		this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+	},
 
-    PlayAudioIntent() {
-        console.log('PlayAudioIntent()');
-        this.$bixbyCapsule.$audioPlayer
-            .play({
-                url: 'http://www.testsounds.com/track39.mp3',
-                format: 'audio/mp3'
-            })
-            .setMetaData({
-                id: 'jovo-audio',
-                title: 'Drum Solo',
-                artist: 'Random',
-                albumArtUrl: 'test.png'
-            })
-            .setDisplayName('Jovo Audio - Drum Solo');
-    },
+	PlayAudioIntent() {
+		console.log('PlayAudioIntent()');
+		this.$bixbyCapsule.$audioPlayer
+			.play({
+				url: 'http://www.testsounds.com/track39.mp3',
+				format: 'audio/mp3'
+			})
+			.setMetaData({
+				id: 'jovo-audio',
+				title: 'Drum Solo',
+				artist: 'Random',
+				albumArtUrl: 'test.png'
+			})
+			.setDisplayName('Jovo Audio - Drum Solo');
+	},
 
-    AudioPlayingIntent() {
-        console.log('AudioPlayingIntent()');
-        this.$layout.single = 'Playing audio - layout style!';
-        this.tell('Playing audio.');
-    }
+	AudioPlayingIntent() {
+		console.log('AudioPlayingIntent()');
+		this.$layout.single = 'Playing audio - layout style!';
+		this.tell('Playing audio.');
+	}
 });
 
 module.exports.app = app;
