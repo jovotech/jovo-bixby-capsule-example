e89e3baf8bd30f065e1370d335c996a0aff1d8a4
rubenaeg <email@aegerter.tech>
2020-02-28 14:55:01 +0100

    :refactor: Adjust capsule to new Jovo v3

--- src/app.js
+++ src/app.js
@@ -33,31 +33,7 @@ app.setHandler({
 
 	MyNameIsIntent() {
 		console.log('MyNameIsIntent()');
-		this.$bixbyCapsule.$layout.single = 'Hello World.';
-		this.$session.$data.name = this.$inputs.name.value;
 		this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
-	},
-
-	PlayAudioIntent() {
-		console.log('PlayAudioIntent()');
-		this.$bixbyCapsule.$audioPlayer
-			.play({
-				url: 'http://www.testsounds.com/track39.mp3',
-				format: 'audio/mp3'
-			})
-			.setMetaData({
-				id: 'jovo-audio',
-				title: 'Drum Solo',
-				artist: 'Random',
-				albumArtUrl: 'test.png'
-			})
-			.setDisplayName('Jovo Audio - Drum Solo');
-	},
-
-	AudioPlayingIntent() {
-		console.log('AudioPlayingIntent()');
-		this.$layout.single = 'Playing audio - layout style!';
-		this.tell('Playing audio.');
 	}
 });
 
